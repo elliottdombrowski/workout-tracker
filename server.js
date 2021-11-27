1748bf2c22063d256c214c608db007f069b84128
elliottdombrowski <elliottroyal@me.com>
2021-11-27 11:20:18 -0600

    wrote basic server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//DECLARING LOCAL/ENV PORT TO LISTEN ON
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+}); 
+
+//TODO- FINISH ROUTES AND FIX REQUIRE PATH
+app.use(require('./routes/'));
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
